37ef1c26e965a49097c751292bebe6c6e1632614
Josh Ford <thisisjoshford@gmail.com>
2020-05-20 15:03:59 -0700

    component wireframe

---
+++ src/components/0_App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function App() {
+  return <h1>Hello World</h1>;
+};
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
