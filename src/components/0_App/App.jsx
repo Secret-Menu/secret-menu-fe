1d8245173cd3683237662c5bba3b87c95f3ea282
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-20 17:39:02 -0700

    add Signup component

--- src/components/0_App/App.jsx
+++ src/components/0_App/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-
-export default function App() {
-  return <h1>Hello World</h1>;
-};
-import React from 'react';
+import SignUp from '../Profiles/Auth/SignUp';
 
 export default function App() {
   return (
     <div>
-      
+      <SignUp />
     </div>
   )
 }
--- src/components/Profiles/Auth/SignUp.jsx
+++ src/components/Profiles/Auth/SignUp.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import request from 'superagent';
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState('User');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // signup function goes here
+  };
+
   return (
     <div>
-      
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={firstName} onChange={({ target }) => setFirstName(target.value)} placeholder="First name" />
+        <input type="text" value={lastName} onChange={({ target }) => setLastName(target.value)} placeholder="Last name" />
+        <input type="text" value={email} onChange={({ target }) => setEmail(target.value)} placeholder="Email address" />
+        <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} placeholder="Create password" />
+        <label htmlFor="user">User</label>
+        <input type="radio" value="User" name="role" id="user" checked={role === 'User'}/>
+        <label htmlFor="restaurant">Restaurant Owner</label>
+        <input type="radio" value="Restaurant" name="role" id="restaurant" checked={role === 'Restaurant'} />
+        <button>Sign Up</button>
+      </form>
     </div>
   );
 }
