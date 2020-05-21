5fcb20db52a56a77a2c5b439a1416bc7b3d7275f
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-20 21:20:51 -0700

    add signup action

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,38 +1,12 @@
-import { postSignUp, postLogIn, getLogOut, getVerify } from '../services/auth-api';
+import { postSignUp } from "../services/auth-api";
 
-export const SET_USER_LOADING = 'SET_USER_LOADING';
-export const SET_USER = 'SET_USER';
-export const SET_USER_ERROR = 'SET_USER_ERROR';
-export const authActionCreatorCreator = authFn => (email, password) => dispatch => {
-  dispatch({
-    type: SET_USER_LOADING
-  });
-  
-  authFn(email, password)
+export const SIGNUP = 'SIGNUP';
+export const signup = (firstName, lastName, email, password, role) => dispatch => {
+  postSignUp(firstName, lastName, email, password, role)
     .then(user => {
       dispatch({
-        type: SET_USER,
+        type: SIGNUP,
         payload: user
-      });
+      })
     })
-    .catch(err => {
-      dispatch({
-        type: SET_USER_ERROR,
-        payload: err.message
-      });
-    });
 };
-
-export const logout = () => dispatch => {
-  getLogOut()
-    .then(() => {
-      dispatch({
-        type: SET_USER,
-        payload: null
-      });
-    });
-};
-
-export const signup = authActionCreatorCreator(postSignUp);
-export const login = authActionCreatorCreator(postLogIn);
-export const verify = authActionCreatorCreator(getVerify);
