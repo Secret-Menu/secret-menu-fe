062da7a40bae4b1de685e50fa0d95543b3d5a6d8
Josh Ford <thisisjoshford@gmail.com>
2020-05-21 14:25:08 -0700

    missing semi

--- src/services/lat-lng-api.js
+++ src/services/lat-lng-api.js
@@ -1,5 +1,5 @@
 export default fetchLatLng(address){
   const data = fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
-  .then(res => res.json());
+  .then(res => console.log(res.json()));
   return data;
 }
