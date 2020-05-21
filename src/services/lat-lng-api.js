bb82a860b392b36a9850b4098df34c31ac90aa10
Josh Ford <thisisjoshford@gmail.com>
2020-05-21 14:39:27 -0700

    latLng api service

--- src/services/lat-lng-api.js
+++ src/services/lat-lng-api.js
@@ -1,5 +1,5 @@
-export default fetchLatLng(address){
+export const fetchLatLng = (address) => {
   const data = fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
-  .then(res => console.log(res.json()));
+    .then(res => res.json());
   return data;
-}
+};
