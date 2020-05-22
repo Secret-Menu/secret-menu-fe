41f24e32d25de2a14a505f84fdfdcf2acd84f3b0
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-21 23:22:00 -0700

    add new line at end of files

--- src/components/Profiles/Auth/RestaurantSignUp.jsx
+++ src/components/Profiles/Auth/RestaurantSignUp.jsx
@@ -163,5 +163,5 @@ export default function RestaurantSignUp() {
         <button>Register</button>
       </form>
     </div>
-  )
-}
+  );
+};
--- src/services/restaurants-api.js
+++ src/services/restaurants-api.js
@@ -24,5 +24,5 @@ export const createRestaurant = restaurant => {
   .then(([ok, json]) => {
     if(!ok) throw json;
     return json;
-  })
-}
+  });
+};
