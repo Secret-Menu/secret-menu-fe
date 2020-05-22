723cd9af899e1dd72ec482b91778c755338d8821
Josh Ford <thisisjoshford@gmail.com>
2020-05-21 21:34:00 -0700

    blank line removal;

--- src/services/restaurantFetch.js
+++ src/services/restaurantFetch.js
@@ -2,5 +2,3 @@ export const getAreaRestaurants = (quadrant) => {
   return fetch(`https://crowd-pleaser-staging.herokuapp.com/api/v1/restaurants?quadrant=${quadrant}`)
     .then(res => res.json());
 };
-
-
