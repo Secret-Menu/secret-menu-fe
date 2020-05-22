fac596c6f9b8c2221e70454ba32b0bcb67384ebf
Josh Ford <thisisjoshford@gmail.com>
2020-05-21 21:33:04 -0700

    fetch quadrant restaurants service

---
+++ src/services/restaurantFetch.js
@@ -0,0 +1,6 @@
+export const getAreaRestaurants = (quadrant) => {
+  return fetch(`https://crowd-pleaser-staging.herokuapp.com/api/v1/restaurants?quadrant=${quadrant}`)
+    .then(res => res.json());
+};
+
+
