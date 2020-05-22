bf95fd3fe70e607a36ccc2fc3d0d3a15510a88c6
Josh Ford <thisisjoshford@gmail.com>
2020-05-21 21:36:17 -0700

    rename function

--- src/services/latlngFetch.js
+++ src/services/latlngFetch.js
@@ -1,4 +1,4 @@
-export const fetchLatLng = (address) => {
+export const getLatLng = (address) => {
   const data = fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GOOGLE_API_KEY}`)
     .then(res => res.json());
   return data;
