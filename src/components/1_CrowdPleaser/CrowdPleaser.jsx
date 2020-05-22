83443323774b35a746a159e1ca44e7a474cdff5d
Josh Ford <thisisjoshford@gmail.com>
2020-05-22 00:22:42 -0700

    linked to map

--- src/components/1_CrowdPleaser/CrowdPleaser.jsx
+++ src/components/1_CrowdPleaser/CrowdPleaser.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useCurrentUser } from '../../hooks/authHooks';
+import { Link } from 'react-router-dom';
 
 export default function CrowdPleaser() {
   const user = useCurrentUser();
   return (
     <div>
       <h1>Welcome to Secret Menu...</h1>
+      <Link to={'/portland'}>ENTER</Link>
     </div>
   );
 }
