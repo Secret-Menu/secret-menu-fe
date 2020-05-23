00829a43386ce9a21c9624a1a82c6a175542a954
Hannah Riley <riley.hannahm@gmail.com>
2020-05-23 15:56:28 -0700

    fetch polls by business complete

--- src/services/business-api.js
+++ src/services/business-api.js
@@ -3,3 +3,9 @@ export const fetchBusinessOfferings = (id) => {
     .then(res => res.json())
     .then(res.offerings);
 };
+
+export const fetchBusinessPolls = (id) => {
+  return fetch(`${process.env.API_URL}/api/v1/restaurants/${id}`)
+    .then(res => res.json())
+    .then(res.polls);
+};
