8dab75071d8cc91662fd98efbc6fc7699338909f
Josh Ford <thisisjoshford@gmail.com>
2020-05-22 23:11:17 -0700

    text header removed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,15 +16,11 @@ const Header = () => {
   
   return (
     <div>
-
       <img src={logo} alt="Crowd Pleaser Logo" style={{height: '100px'}}></img>
       <Link to="/"> <button>Home</button> </Link>
-      {/* not sure what our all restaurant route is */}
       <Link to="/portland"> <button>Restaurants</button> </Link>
-      <h1>Crowd Pleaser</h1>
       { user && <Link to="/"> <button onClick={handleLogOut}>Log Out</button> </Link>}
       { !user && <LogInSignUp />}
-      
     </div>
   );
 };
