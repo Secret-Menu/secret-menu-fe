2642660c08139c72f370ae3afbe506b7465aac87
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-23 15:26:06 -0700

    cartReducer test passing for REMOVE_FROM_CART action

--- src/reducers/cartReducer.test.js
+++ src/reducers/cartReducer.test.js
@@ -30,6 +30,26 @@ describe('cartReducer tests', () => {
         pickUpDate: 'May 5, 2021',
         total: 4000
       }
-    ])
+    ]);
+  });
+
+  it('handles the REMOVE_FROM_CART action', () => {
+    const state = [{
+      restaurant: 'Chris\' Bamboo Grove',
+      offering: 'Shoyu Chicken Mixed Plate',
+      quantity: 2,
+      price: 2000,
+      pickUpDate: 'May 5, 2021',
+      total: 4000
+    }];
+
+    const action = {
+      type: REMOVE_FROM_CART,
+      payload: 0
+    };
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([]);
   })
 })
