8bc69304d50eccaee69637e67f816370a7f56379
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-23 15:00:29 -0700

    cartReducer switch statement setup

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -6,6 +6,10 @@ export default function reducer(state = [], action) {
       return [...state, action.payload];
     case REMOVE_FROM_CART:
       return state.filter((lineItem, i) => i !== action.payload);
+    case UPDATE_CART_ITEM:
+      return state.map((lineItem, i) => {
+        if(i === action.payload.index) return action.payload.lineItem
+      });
     default:
       return state;
   }
