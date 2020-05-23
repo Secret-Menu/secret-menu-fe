a490bc7443fb4eaf955a1dc7177dd99620ca68c0
Josh Ford <thisisjoshford@gmail.com>
2020-05-22 22:34:14 -0700

    get all restuarants selctor

--- src/selectors/restaurantSelectors.js
+++ src/selectors/restaurantSelectors.js
@@ -1,3 +1,5 @@
+export const getAllRestaurants = state => state.restaurants.allRestaurants;
 export const getAreaSelector = state => state.restaurants.areaRestaurants;
 export const getRestaurant = state => state.restaurants.restaurant;
+
 
