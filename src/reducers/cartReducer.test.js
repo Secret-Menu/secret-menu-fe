80234861fd49b62aa1ad39d2f41162f5aa2b5871
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-23 15:22:46 -0700

    cartReducer test passing for ADD_TO_CART

---
+++ src/reducers/cartReducer.test.js
@@ -0,0 +1,35 @@
+import reducer from './cartReducer';
+import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_CART_ITEM } from '../actions/cartActions';
+
+describe('cartReducer tests', () => {
+  it('handles ADD_TO_CART action', () => {
+    const state = [];
+
+    const lineItem = {
+      restaurant: 'Chris\' Bamboo Grove',
+      offering: 'Shoyu Chicken Mixed Plate',
+      quantity: 2,
+      price: 2000,
+      pickUpDate: 'May 5, 2021',
+      total: 4000
+    };
+
+    const action = {
+      type: ADD_TO_CART,
+      payload: lineItem
+    };
+
+    const newState = reducer(state, action);
+
+    expect(newState).toEqual([
+      {
+        restaurant: 'Chris\' Bamboo Grove',
+        offering: 'Shoyu Chicken Mixed Plate',
+        quantity: 2,
+        price: 2000,
+        pickUpDate: 'May 5, 2021',
+        total: 4000
+      }
+    ])
+  })
+})
