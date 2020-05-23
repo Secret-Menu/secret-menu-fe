b8efe9bc5920ca06379a0fb086d682cbc3b99916
Josh Ford <thisisjoshford@gmail.com>
2020-05-22 23:22:02 -0700

    map rendering ALL RESTAURANTS!!!

--- src/components/2_PDXMap/PDXMap.jsx
+++ src/components/2_PDXMap/PDXMap.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Map from '../Map/Map';
 import { Link } from 'react-router-dom';
 import { pdxAnchor } from '../../services/map-utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllRestaurants } from '../../actions/restaurantActions';
+import { selectAllRestaurants } from '../../selectors/restaurantSelectors';
 
 export default function PDXMap() {  
+  const dispatch = useDispatch();
+  const restaurants = useSelector(selectAllRestaurants);
 
+  useEffect(() => {
+    dispatch(getAllRestaurants());
+  }, []);
 
   return (
     <div>
-
       <div style={{ height: '60vh', width: '50%' }}>
-        <Map center={pdxAnchor.center} zoom={pdxAnchor.zoom} markers={[]}/>
+        <Map center={pdxAnchor.center} zoom={pdxAnchor.zoom} markers={restaurants}/>
         <Link to={'/portland/Northwest'}>NW</Link>
       </div>
     </div>
