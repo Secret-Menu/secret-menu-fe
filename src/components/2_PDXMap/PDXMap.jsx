dcc0b2f5f4f35f1860d52c2da7f69327f7086a27
Josh Ford <thisisjoshford@gmail.com>
2020-05-22 23:12:25 -0700

    removed logo from map component

--- src/components/2_PDXMap/PDXMap.jsx
+++ src/components/2_PDXMap/PDXMap.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import Map from '../Map/Map';
 import { Link } from 'react-router-dom';
 import { pdxAnchor } from '../../services/map-utils';
-import logo from '../../assets/mainLogo.png';
 
 export default function PDXMap() {  
 
 
   return (
     <div>
-      
+
       <div style={{ height: '60vh', width: '50%' }}>
         <Map center={pdxAnchor.center} zoom={pdxAnchor.zoom} markers={[]}/>
         <Link to={'/portland/Northwest'}>NW</Link>
