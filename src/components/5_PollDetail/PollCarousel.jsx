671f1f6443358f64337fd7353e6902e0b4de3033
cficht <chris.ficht@gmail.com>
2020-05-23 21:45:31 -0700

    Carousel transition idea

--- src/components/5_PollDetail/PollCarousel.jsx
+++ src/components/5_PollDetail/PollCarousel.jsx
@@ -5,18 +5,27 @@ import styles from './PollCarousel.css';
 
 const PollCarousel = ({ polls }) => {
   const [current, setCurrent] = useState(0);
+  const [currentStyle, setCurrentStyle] = useState(styles.BeforeFade);
   const carouselMax = polls.length - 1;
 
   const handleCarousel = (change) => {
+    if(change === -1) setCurrentStyle(styles.Right);
+    if(change === 1) setCurrentStyle(styles.Left);
+    setTimeout(() => {
+      setCurrentStyle(styles.BeforeFade);
+    }, 1000);
+
     setCurrent(current + change);
   };
 
   if(polls.length > 0) { 
     return (
       <div className={styles.PollCarousel}>
-        <button onClick={() => handleCarousel(-1)} disabled={current === 0}>&larr;</button>
-        <PollDetail poll={polls[current]} key={polls[current]._id}/>
-        <button onClick={() => handleCarousel(+1)} disabled={current === carouselMax}>&rarr;</button>
+        <button name="left" onClick={() => handleCarousel(-1)} disabled={current === 0}>&larr;</button>
+        <div className={currentStyle}>
+          <PollDetail poll={polls[current]} key={polls[current]._id}/>
+        </div>
+        <button name="right" onClick={() => handleCarousel(+1)} disabled={current === carouselMax}>&rarr;</button>
       </div>
     ); 
   } else { 
