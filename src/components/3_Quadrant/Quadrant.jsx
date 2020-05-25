d63a3a5528c30704153f45b60b36e6b9221786e9
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-24 22:08:44 -0700

    remove deleted selector import from Quadrant

--- src/components/3_Quadrant/Quadrant.jsx
+++ src/components/3_Quadrant/Quadrant.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Map from '../Map/Map';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAreaRestaurants } from '../../actions/restaurantActions';
-import { selectAreaRestaurants, selectAreaRestaurantsOnly } from '../../selectors/restaurantSelectors';
+import { selectAreaRestaurants } from '../../selectors/restaurantSelectors';
 import { useParams } from 'react-router-dom';
 import RestaurantList from './RestaurantList';
 
