0d49e4d5b7bbb831d8e81e1a817a3fe31f0729c8
cficht <chris.ficht@gmail.com>
2020-05-26 13:16:31 -0700

    fixed index.js and App with user profile

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,12 @@ import restaurants from './restaurantReducer';
 import cart from './cartReducer';
 import userProfile from './userProfileReducer';
 import business from './businessReducer';
-import orders from './ordersReducer';
+// import orders from './ordersReducer';
 
 export default combineReducers({
   auth,
   restaurants,
   cart,
-  userProfile
-  business,
-  orders
+  userProfile,
+  business
 });
