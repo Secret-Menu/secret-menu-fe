616ca50c73b985ab296d1fbc001555dbc38f7c24
Hannah Riley <riley.hannahm@gmail.com>
2020-05-25 18:02:13 -0700

    business orders selector passing

--- src/selectors/businessSelectors.js
+++ src/selectors/businessSelectors.js
@@ -3,3 +3,5 @@ export const selectRestaurant = state => state.auth.user.restaurant[0];
 export const selectPolls = state => state.auth.user.restaurant[0].polls;
 
 export const selectOfferings = state => state.auth.user.restaurant[0].offerings;
+
+export const selectOrders = state => state.auth.user.restaurant[0].orders;
--- src/selectors/businessSelectors.test.js
+++ src/selectors/businessSelectors.test.js
@@ -1,4 +1,4 @@
-import { selectRestaurant, selectPolls, selectOfferings } from './businessSelectors';
+import { selectRestaurant, selectPolls, selectOfferings, selectOrders } from './businessSelectors';
 
 describe('business selectors tests', () => {
   it('selects the users restaurant', () => {
@@ -127,6 +127,39 @@ describe('business selectors tests', () => {
   }
     const userOfferings = selectOfferings(state);
     expect(userOfferings).toEqual([])
+  })
+  it('selects the users restaurants orders', () => {
+    const state = {
+        auth: {
+          user: { 
+            restaurant: [{
+              address: { 
+                streetAddress: 'ab',
+                city: 'ab',
+                state: 'OR',
+                zipcode: 11111
+              },
+              _id: '5ecc5dd2031fef2cdce45cee',
+              owner: '5ecc5dbd031fef2cdce45ced',
+              restaurantName: 'ab',
+              email: 'ab@ab',
+              phoneNumber: '111-111-1112',
+              description: 'ab',
+              category: 'Indian',
+              lat: 35.0743187,
+              lng: -90.0861023,
+              quadrant: 'S',
+              websiteUrl: '1',
+              imageUrl: 'ab',
+              offerings: [],
+              polls: [],
+              orders: []
+        }]
+      }
+    }
+  }
+    const userOrders = selectOrders(state);
+    expect(userOrders).toEqual([])
   })
 
 
