c70c51c5142a9eaedca075b84126e68114234aa4
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-26 12:39:05 -0700

    PDXQuadrants successfully rendering quadrant restaurant lists

--- src/components/2_PDXMap/PDXQuadrants.jsx
+++ src/components/2_PDXMap/PDXQuadrants.jsx
@@ -2,38 +2,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import RestaurantList from '../3_Quadrant/RestaurantList';
 
-export default function PDXQuadrants({restaurants}) {
+export default function PDXQuadrants({ restaurants }) {
+  const allRestaurants = restaurants.restaurants;
+
+  const northRestaurants = allRestaurants.filter(restaurant => restaurant?.quadrant === "N");
+  const northEastRestaurants = allRestaurants?.filter(restaurant => restaurant.quadrant === "NE");
+  const northWestRestaurants = allRestaurants?.filter(restaurant => restaurant.quadrant === "NW");
+  const southRestaurants = allRestaurants?.filter(restaurant => restaurant.quadrant === "S");
+  const southEastRestaurants = allRestaurants?.filter(restaurant => restaurant.quadrant === "SE");
+  const southWestRestaurants = allRestaurants?.filter(restaurant => restaurant.quadrant === "SW");
+
   const quadrants = [
     {
       name: 'North',
-      abbrev: 'N'
+      abbrev: 'N',
+      restaurants: northRestaurants
     },
     {
       name: 'Northeast',
-      abbrev: 'N'
+      abbrev: 'N',
+      restaurants: northEastRestaurants
     },
     {
       name: 'Northwest',
-      abbrev: 'NW'
+      abbrev: 'NW',
+      restaurants: northWestRestaurants
     },
     {
       name: 'South',
-      abbrev: 'S'
+      abbrev: 'S',
+      restaurants: southRestaurants
     },
     {
       name: 'Southeast',
-      abbrev: 'SE'
+      abbrev: 'SE',
+      restaurants: southEastRestaurants
     },
     {
       name: 'Southwest',
-      abbrev: 'SW'
+      abbrev: 'SW',
+      restaurants: southWestRestaurants
     }
   ];
-
+  
   const quadrantLists = quadrants.map(quadrant => 
     <div>
       <Link to={`/portland/${quadrant.abbrev}`}>{quadrant.name}</Link>
-      <RestaurantList restaurants={restaurants.restaurants} />
+      <RestaurantList restaurants={quadrant.restaurants} />
     </div>
   );
 
