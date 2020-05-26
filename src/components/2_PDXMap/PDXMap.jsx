c9e29c5095123b811ac8f8531026e27da91afa2d
Josh Ford <thisisjoshford@gmail.com>
2020-05-26 12:47:43 -0700

    cleanup

--- src/components/2_PDXMap/PDXMap.jsx
+++ src/components/2_PDXMap/PDXMap.jsx
@@ -19,11 +19,7 @@ export default function PDXMap() {
   return (
     <div>
       <div style={{ height: '60vh', width: '50%' }}>
-        <button onClick={() => toast.error('⚠️ NOOOOO!!!!')}>Error</button>
-        <button onClick={() => toast.success('Order Placed!')}>Success</button>
-        <button onClick={() => toast('Welcome to Secret Menu!')}>Welcome</button>
         <Map center={pdxAnchor.center} zoom={pdxAnchor.zoom} markers={restaurants}/>
-        <Link to={'/portland/NW'}>NW</Link>
       </div> 
     </div>
   );
