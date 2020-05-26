200313f12f20e9425489123a67bd445e71675464
Josh Ford <thisisjoshford@gmail.com>
2020-05-26 12:48:43 -0700

    remove link and toast from map

--- src/components/2_PDXMap/PDXMap.jsx
+++ src/components/2_PDXMap/PDXMap.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import Map from '../Map/Map';
-import { Link } from 'react-router-dom';
 import { pdxAnchor } from '../../services/map-utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllRestaurants } from '../../actions/restaurantActions';
 import { selectAllRestaurants } from '../../selectors/restaurantSelectors';
-import { toast } from 'react-toastify';
 
 export default function PDXMap() { 
 
