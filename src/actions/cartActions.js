aacea2e269892807248b4702d1db4485a4084416
Josh Ford <thisisjoshford@gmail.com>
2020-05-27 14:11:20 -0700

    add, update, and delete cart items toast notifications

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,8 +1,10 @@
+import { toast } from 'react-toastify';
+
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const addToCart = lineItem => ({
   type: ADD_TO_CART,
   payload: lineItem
-});
+}).then(toast.success('item added to cart!'));
 
 export const UPDATE_CART_ITEM = 'UPDATE_CART';
 export const updateCartItem = (index, lineItem) => ({
@@ -11,10 +13,10 @@ export const updateCartItem = (index, lineItem) => ({
     index,
     lineItem
   }
-});
+}).then(toast.success('item updated!'));
 
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const removeFromCart = index => ({
   type: REMOVE_FROM_CART,
   payload: index
-});
+}).then(toast.success('item removed!'));
