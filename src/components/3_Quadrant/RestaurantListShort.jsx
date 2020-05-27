6b7958a9ec595ad8c3e284915cdaf7b1993f61ad
Jenna Goldman <jenna.goldman89@gmail.com>
2020-05-26 21:38:52 -0700

    add RestaurantListShort component and custom hook

--- src/components/3_Quadrant/RestaurantListShort.jsx
+++ src/components/3_Quadrant/RestaurantListShort.jsx
@@ -1,37 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './RestaurantList.css';
+import { useRestaurantShortList } from '../../hooks/restaurantHooks';
 
-const RestaurantList = ({ restaurants }) => {
-
+const RestaurantListShort = ({ restaurants }) => {
   const restaurantsCopy = restaurants.slice();
 
-  const shuffle = restaurantsCopy => {
-    let ctr = restaurantsCopy.length, temp, index;
-    while (ctr > 0) {// Pick a random index
-        index = Math.floor(Math.random() * ctr);
-        ctr--;
-        temp = restaurantsCopy[ctr];
-        restaurantsCopy[ctr] = restaurantsCopy[index];
-        restaurantsCopy[index] = temp;
-    };
-    return restaurantsCopy;
-};
+  const {
+    restaurantShortList
+  } = useRestaurantShortList(restaurantsCopy);
 
-console.log(shuffle(restaurantsCopy));
-
-  // if(restaurants.length > 5) {
-
-  // }
-  const restaurantNodes = restaurants.map(item =>
-  <li key={item._id}>
-    <Link to={`/restaurant/${item._id}`}>
-      <div>
-        <p>{item.restaurantName}</p>
-        <img src={item.imageUrl} alt={item.restaurantName} className={styles.restaurantImg} />
-      </div>
-    </Link>
-  </li>);
+  const restaurantNodes = restaurantShortList.map(item =>
+    <li key={item._id}>
+      <Link to={`/restaurant/${item._id}`}>
+        <div>
+          <p>{item.restaurantName}</p>
+          <img src={item.imageUrl} alt={item.restaurantName} className={styles.restaurantImg} />
+        </div>
+      </Link>
+    </li>
+  );
 
   return (
     <ul>
@@ -40,4 +28,4 @@ console.log(shuffle(restaurantsCopy));
   );
 };
 
-export default RestaurantList;
+export default RestaurantListShort;
