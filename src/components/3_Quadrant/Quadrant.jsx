c4bb5b83a1a31d39a808a7852cb28b3debd2b362
cficht <chris.ficht@gmail.com>
2020-05-27 14:47:25 -0700

    quadrant polls

--- src/components/3_Quadrant/Quadrant.jsx
+++ src/components/3_Quadrant/Quadrant.jsx
@@ -6,11 +6,30 @@ import { selectAreaRestaurants } from '../../selectors/restaurantSelectors';
 import { useParams } from 'react-router-dom';
 import { useQuadrantName } from '../../hooks/quadrantHooks';
 import RestaurantList from './RestaurantList';
+import PollCarousel from '../5_PollDetail/PollCarousel';
 
 export default function Quadrant(){
   const dispatch = useDispatch();
   const { area } = useParams();
   const restaurants = useSelector(selectAreaRestaurants);
+  console.log(restaurants.restaurants);
+
+  const quadrantPolls = restaurants.restaurants.map(restaurant => (
+    restaurant.polls
+  )).flat();
+  console.log(quadrantPolls);
+
+  // return state.userProfile.userOrders.map(order => {
+  //   if(!order.offering) return;
+  //   return order.offering.map(offering => ({
+  //     ...offering,
+  //     restaurant: order.restaurant,
+  //     orderNumber: order.orderNumber,
+  //     orderTotal: order.orderTotal,
+  //     user: order.user,
+  //     created_at: order.created_at
+  //   }));
+  // }).flat()
 
   const {
     quadrantName
@@ -20,6 +39,8 @@ export default function Quadrant(){
     dispatch(getAreaRestaurants(area));
   }, []);
 
+
+
   return (
     <div>
       <h2>{quadrantName}</h2>
@@ -27,6 +48,9 @@ export default function Quadrant(){
         {restaurants.anchorPoint.zoom && 
         <Map center={restaurants.anchorPoint.center} zoom={restaurants.anchorPoint.zoom} markers={restaurants} />}
         {console.log(restaurants.anchorPoint.center)}
+      </div>
+      <div>
+        <PollCarousel polls={quadrantPolls}/>
       </div>
       <RestaurantList restaurants={restaurants.restaurants}/>
     </div>
