7e3d5b1591793161319fe6800835b6b726075e74
Josh Ford <thisisjoshford@gmail.com>
2020-05-27 14:17:08 -0700

    lint fix

--- src/components/Cart/CheckOut.jsx
+++ src/components/Cart/CheckOut.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './Cart';
-import CheckOutForm from './CheckoutForm';
 import NoCart from './NoCart';
 import { selectCart } from '../../selectors/cartSelectors';
 
@@ -9,16 +8,16 @@ export default function CheckOut() {
 
   const cart = useSelector(selectCart);
 
-  cart.length > 0
+  cart.length > 0;
 
   return (
     <>
       { cart.length > 0
-          ? <>
-              <Cart />
-            </>
-          : <NoCart />
+        ? <>
+          <Cart />
+        </>
+        : <NoCart />
       }
     </>
-  )
+  );
 }
