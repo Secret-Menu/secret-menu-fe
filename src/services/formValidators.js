fe3ec1be3c96a3a0131c2b4ceeee2614795ce276
Josh Ford <thisisjoshford@gmail.com>
2020-05-28 16:03:51 -0700

    updated error msgs

--- src/services/formValidators.js
+++ src/services/formValidators.js
@@ -2,31 +2,31 @@ export const validateSignUp = (newUser) => {
   const errors = [];
 
   if(newUser.firstName.length === 0) {
-    errors.push('First Name Missing  ⚠️');
+    errors.push('FIRST NAME Missing  ⚠️');
   }
   if(newUser.lastName.length === 0) {
-    errors.push('Last Name Missing ⚠️');
+    errors.push('LAST NAME Missing ⚠️');
   }
   if(newUser.email && newUser.email.length < 5) {
-    errors.push('Email is too short ⚠️');
+    errors.push('EMAIL is too short ⚠️');
   }
   if(newUser.email && newUser.email
     .split('')
     .filter(x => x === '@')
     .length !== 1) {
-    errors.push('Email is Missing an @ symbol ⚠️');
+    errors.push('EMAIL is Missing an @ symbol ⚠️');
   }
   if(newUser.email && newUser.email.indexOf('.') === -1) {
-    errors.push('Email is Missing a dot ⚠️');
+    errors.push('EMAIL is Missing a dot ⚠️');
   }
   if(!newUser.email) {
-    errors.push('Missing Email ⚠️');
+    errors.push('EMAIL Missing  ⚠️');
   }
   if(!newUser.password) {
-    errors.push('Password Missing ⚠️');
+    errors.push('PASSWORD Missing ⚠️');
   }
   if(!newUser && newUser.password.length < 4) {
-    errors.push('Password should be at least 4 characters long ⚠️');
+    errors.push('PASSWORD should be at least 4 characters long ⚠️');
   }
   return errors;
 };
@@ -35,34 +35,34 @@ export const validateBusiness = (newBusiness) => {
   const errors = [];
 
   if(newBusiness.restaurantName.length === 0) {
-    errors.push('Business Name Missing ⚠️');
+    errors.push('Business NAME Missing ⚠️');
   }
   if(!newBusiness.address.streetAddress) {
-    errors.push('Street Address Invalid ⚠️');
+    errors.push('STREET ADDRESS Invalid ⚠️');
   }
   if(!newBusiness.address.city) {
-    errors.push('City Invalid ⚠️');
+    errors.push('CITY Missing ⚠️');
   }
   if(!newBusiness.address.zipcode) {
-    errors.push('Zip Code Invalid ⚠️');
+    errors.push('ZIP CODE Invalid ⚠️');
   }
   if(!newBusiness.phoneNumber) {
-    errors.push('Phone Number Invalid ⚠️');
+    errors.push('PHONE Invalid ⚠️');
   }
   if(!newBusiness.websiteUrl) {
-    errors.push('Website Invalid ⚠️');
+    errors.push('WEBSITE Missing ⚠️');
   }
   if(!newBusiness.email) {
-    errors.push('Email Invalid ⚠️');
+    errors.push('EMAIL Missing ⚠️');
   }
   if(!newBusiness.description) {
     errors.push('Please enter a description ⚠️');
   }
   if(!newBusiness.quadrant) {
-    errors.push('Please select a quadrant ⚠️');
+    errors.push('QUADRANT Missing ⚠️');
   }
   if(!newBusiness.imageUrl) {
-    errors.push('Please enter an image url ⚠️');
+    errors.push('IMAGE URL Missing ⚠️');
   }
   return errors; 
 };
@@ -71,28 +71,28 @@ export const validateOffering = (offering) => {
   const errors = [];
 
   if(!offering.dishName) {
-    errors.push('Please enter a Name ⚠️');
+    errors.push('NAME missing ⚠️');
   }
   if(!offering.imageUrl) {
-    errors.push('Image url missing ⚠️');
+    errors.push('IMAGE URL missing ⚠️');
   }
   if(!offering.description) {
     errors.push('Please enter a description ⚠️');
   }
   if(!offering.numRemaining) {
-    errors.push('Please enter number of offerings ⚠️');
+    errors.push('NUMBER of offerings missing⚠️');
   }
   if(!offering.numRemaining || isNaN(offering.numRemaining)) {
-    errors.push('Number of offerings must be a number ⚠️');
+    errors.push('NUMBER of offerings must be a number ⚠️');
   }
   if(offering.numRemaining < 1) {
-    errors.push('Number of offerings must be greater than 0 ⚠️');
+    errors.push('NUMBER of offerings must be greater than 0 ⚠️');
   }
   if(!offering.pickUpDate) {
     errors.push('Please enter a pickup date ⚠️');
   }
   if(!offering.price) {
-    errors.push('Please enter a price ⚠️');
+    errors.push('PRICE Missing ⚠️');
   }
   if(!offering.price && isNaN(offering.price)) {
     errors.push('Offering price must be a number ⚠️');
@@ -105,39 +105,39 @@ export const validatePoll = (poll) => {
   const errors = [];
 
   if(!poll.name) {
-    errors.push('Please enter a Poll Name ⚠️');
+    errors.push('Missing NAME for Poll ⚠️');
   }
   if(!poll.offering1Name) {
     errors
-      .push('Please enter a name for: Offering 1 ⚠️');
+      .push('Missing NAME: Offering 1 ⚠️');
   }
   if(!poll.offering2Name) {
     errors
-      .push('Please enter a name for: Offering 2 ⚠️');
+      .push('Missing NAME: Offering 2 ⚠️');
   }
   if(!poll.name) {
-    errors.push('Please enter a Name ⚠️');
+    errors.push('Missing POLL NAME ⚠️');
   }
   if(!poll.offering1ImageUrl) {
-    errors.push('Image url missing (Offering 1) ⚠️');
+    errors.push('Missing IMAGE (Offering 1) ⚠️');
   }
   if(!poll.offering2ImageUrl) {
-    errors.push('Image url missing (Offering 2)⚠️');
+    errors.push('Missing IMAGE (Offering 2)⚠️');
   }
   if(!poll.offering1Description) {
-    errors.push('Please enter a description (Offering 1) ⚠️');
+    errors.push('Missing DESCRIPTION (#1) ⚠️');
   }
   if(!poll.offering2Description) {
-    errors.push('Please enter a description (Offering 1) ⚠️');
+    errors.push('Missing DESCRIPTION (#2) ⚠️');
   }
   if(!poll.start) {
-    errors.push('Please enter start date for poll ⚠️');
+    errors.push('Missing START date ⚠️');
   }
   if(!poll.end) {
-    errors.push('Please enter an end date for poll ⚠️');
+    errors.push('Missing END date ⚠️');
   }
   if(poll.status) {
-    errors.push('Enter a status of Open or Closed ⚠️');
+    errors.push('Missing STATUS ⚠️');
   }
   
   return errors; 
