0d4c85a4c0ee6b9bde5367b21464780fae05957b
Josh Ford <thisisjoshford@gmail.com>
2020-05-27 22:58:55 -0700

    breakdown

--- src/components/Profiles/Auth/SignUp.jsx
+++ src/components/Profiles/Auth/SignUp.jsx
@@ -18,14 +18,52 @@ export default function SignUp() {
     <div className={styles.Form}>
       <h2>Sign Up</h2>
       <form onSubmit={handleSignUp}>
-        <input type="text" value={firstName} name="firstName" onChange={handleChange} placeholder="First name" />
-        <input type="text" value={lastName} name="lastName" onChange={handleChange} placeholder="Last name" />
-        <input type="text" value={email} name="email" onChange={handleChange} placeholder="Email address" />
-        <input type="password" value={password} name="password" onChange={handleChange} placeholder="Create password" />
+        <input 
+          type="text" 
+          value={firstName} 
+          name="firstName" 
+          onChange={handleChange} 
+          placeholder="First name" 
+        />
+        <input 
+          type="text" 
+          value={lastName} 
+          name="lastName" 
+          onChange={handleChange} 
+          placeholder="Last name" 
+        />
+        <input 
+          type="text" 
+          value={email} 
+          name="email" 
+          onChange={handleChange} 
+          placeholder="Email address" 
+        />
+        <input 
+          type="password" 
+          value={password} 
+          name="password" 
+          onChange={handleChange} 
+          placeholder="Create password" 
+        />
         <label htmlFor="user">User</label>
-        <input type="radio" value="User" name="role" id="user" onChange={handleChange} checked={role === 'User'}/>
+        <input 
+          type="radio" 
+          value="User" 
+          name="role" 
+          id="user" 
+          onChange={handleChange} 
+          checked={role === 'User'}
+        />
         <label htmlFor="restaurant">Restaurant Owner</label>
-        <input type="radio" value="Restaurant" name="role" id="restaurant" onChange={handleChange} checked={role === 'Restaurant'} />
+        <input 
+          type="radio" 
+          value="Restaurant" 
+          name="role" 
+          id="restaurant" 
+          onChange={handleChange}
+          checked={role === 'Restaurant'} 
+        />
         <button disabled={loading}>Sign Up</button>
       </form>
     </div>
