f609c051c8bb100e7a529ecfcf08aa52a5f95a79
Josh Ford <thisisjoshford@gmail.com>
2020-05-27 23:13:48 -0700

    breakdown / default selected

--- src/components/Profiles/BusinessProfile/Forms/AddPoll.jsx
+++ src/components/Profiles/BusinessProfile/Forms/AddPoll.jsx
@@ -56,17 +56,74 @@ export default function AddPoll() {
   return (
     <div className={styles.Form}>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} name="pollName" onChange={handleChange} placeholder="Poll Name" />
-        <input type="text" value={offering1Name} name="offering1Name" onChange={handleChange} placeholder="First Offering" />
-        <input type="text" value={offering2Name} name="offering2Name" onChange={handleChange} placeholder="Second Offering" />
-        <input type="date" value={start} name="start" onChange={handleChange} placeholder="Start date" />
-        <input type="date" value={end} name="end" onChange={handleChange} placeholder="End date" />
-        <input type="text" value={offering1Description} name="offering1Description" onChange={handleChange} placeholder="First offering description" />
-        <input type="text" value={offering2Description} name="offering2Description" onChange={handleChange} placeholder="First offering description" />
-        <input type="text" value={offering1ImageUrl} name="offering1ImageUrl" onChange={handleChange} placeholder="First offering image url" />
-        <input type="text" value={offering2ImageUrl} name="offering2ImageUrl" onChange={handleChange} placeholder="Second offering image url" />
-        <select value={status} name="status" onChange={handleChange}>
-          <option value="open">Open</option>
+        <input 
+          type="text" 
+          value={name} 
+          name="pollName" 
+          onChange={handleChange} 
+          placeholder="Poll Name" 
+        />
+        <input 
+          type="text" 
+          value={offering1Name} 
+          name="offering1Name" 
+          onChange={handleChange} 
+          placeholder="First Offering" 
+        />
+        <input 
+          type="text" 
+          value={offering2Name} 
+          name="offering2Name" 
+          onChange={handleChange} 
+          placeholder="Second Offering" 
+        />
+        <input 
+          type="date" 
+          value={start} 
+          name="start" 
+          onChange={handleChange} 
+          placeholder="Start date"
+        />
+        <input 
+          type="date" 
+          value={end} 
+          name="end" 
+          onChange={handleChange} 
+          placeholder="End date" 
+        />
+        <input 
+          type="text" 
+          value={offering1Description} 
+          name="offering1Description" 
+          onChange={handleChange} 
+          placeholder="First offering description" 
+        />
+        <input 
+          type="text" 
+          value={offering2Description} 
+          name="offering2Description" 
+          onChange={handleChange} 
+          placeholder="First offering description" 
+        />
+        <input 
+          type="text" 
+          value={offering1ImageUrl} 
+          name="offering1ImageUrl" 
+          onChange={handleChange} 
+          placeholder="First offering image url" 
+        />
+        <input type="text" 
+          value={offering2ImageUrl} 
+          name="offering2ImageUrl" 
+          onChange={handleChange} 
+          placeholder="Second offering image url" 
+        />
+        <select 
+          value={status} 
+          name="status" 
+          onChange={handleChange}
+        >
+          <option value="open" selected>Open</option>
           <option value="closed">Closed</option>
         </select>
         <button>Add</button>
